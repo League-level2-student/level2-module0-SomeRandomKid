e25b72a050305ffdd96ca54ed5b7c7ae8c25cd9a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-05 17:42:54 -0800

    hi

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,25 +1,43 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
 public static void main(String[] args) {
 	//2. create an array of 5 robots.
-	Robot[] boi = new Robot[5];
+	Robot[] boi = new Robot[8];
 	//3. use a for loop to initialize the robots.
 	for (int i = 0; i < boi.length; i++) {
 		boi[i] = new Robot();
+		boi[i].moveTo(250+ i*100, 550);
 	}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-
+	boolean ayy = false; 
+	while (ayy == false) {
+	for (int j = 0; j < boi.length; j++) {
+		boi[j].setSpeed(100);
+		Random ran = new Random();
+		int esketit = ran.nextInt(50);
+		boi[j].move(esketit);
+		if(boi[j].getY() <= 50) {
+			ayy = true;
+			JOptionPane.showMessageDialog(null, "Robot #" + j + " wins the race!!!");
+			break;
+		}
+		}
+	}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 	//7. declare that robot the winner and throw it a party!
-
+	
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -20,9 +20,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] boi;
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -33,24 +33,31 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String ayy = JOptionPane.showInputDialog("Type in a positive number.");
+		int esketit = Integer.parseInt(ayy);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		boi = new JButton[esketit];
 		//5. Make a for loop to iterate through the JButton array
+		for (int i = 0; i < boi.length; i++) {
+			boi[i] = new JButton();
+			boi[i].addActionListener(this);
+			panel.add(boi[i]);
+		}
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
 		
 		//9 add the panel to the window
-		
+		window.add(panel);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		window.setExtendedState(window.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null, "Pick the right button, if you don't...idk what'll happen.");
 		//13. initialize the hiddenButton variable to a random number less than the int created int step 3
-		
+		Random oof = new Random();
+		 hiddenButton = oof.nextInt(esketit - 3);
 		//14. Set the text of the JButton located at hiddenButton the read "ME"
 
 		//15. Use Thread.sleep(100); to pause the program.
